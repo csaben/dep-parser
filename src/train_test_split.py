3d7441085d85f86a57197a2931eed4be5ab6d490
csaben <clark@saben.org>
2023-01-11 15:17:46 -0500

    dataloader hates my collate fn

--- src/train_test_split.py
+++ src/train_test_split.py
@@ -6,6 +6,9 @@ from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 from torch.utils.data import random_split
 from torch.nn.utils.rnn import pad_sequence
+import torch
+import torch.nn as nn
+from torch.nn import MSELoss, CrossEntropyLoss
 
 #def custom_pad_label(labels):
 #    #get the max element lens per label in batch in the form [maxlen tup1, maxlen conn, maxlen tup2]
@@ -28,7 +31,9 @@ from torch.nn.utils.rnn import pad_sequence
 #hail mary collate fn; seems like it doesn't error out-> great! the masking fn is kinda a black box
 def custom_collate_fn(batch):
     sentences, labels = zip(*batch)
-    sentences = [torch.tensors(s) for s in sentences]
+    #sentences needs to be encoded into a numerical representation
+    # sentences = [torch.tensor(s) for s in sentences]
+    sentences = [TOKENIZER.encode(sentence) for sentence in sentences]
     padded_sentences = pad_sequence(sentences, batch_first=True)
 
     #Find the max lengths of each sub-element of the label tuple
@@ -42,10 +47,14 @@ def custom_collate_fn(batch):
 
     #pad labels
     padded_labels = [[[np.pad(sub_elem, (0, max_len - len(sub_elem)), 'constant') if isinstance(sub_elem, np.ndarray) else sub_elem for sub_elem in elem] if isinstance(elem, tuple) else np.pad(elem, (0, max_len - len(elem)), 'constant') for elem in example] for example in batch]
+    print(padded_sentences)
+    print(padded_labels)
+    print(mask)
     
     return padded_sentences, padded_labels, mask
 
 
+# class ComplexTupleLoss(nn.Module)
 
 with open(DATA, 'r') as file:
     text = file.read()
@@ -60,17 +69,27 @@ train_size = len(dataset) - val_size
 train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
 
 # Create DataLoader for training and validation sets
-train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=4,
+train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=2,
                               collate_fn=custom_collate_fn)
-val_dataloader = DataLoader(val_dataset, batch_size=32, shuffle=False, num_workers=4,
+val_dataloader = DataLoader(val_dataset, batch_size=32, shuffle=False, num_workers=2,
                             collate_fn=custom_collate_fn)
 
 
 
-for i, (sentences, tokenized_labels) in enumerate(train_dataloader):
-    print(sentences)
-    print(labels)
-    break
+print(len(train_dataloader))
+"""the custom collate fn is causing problems for my dataloader
+
+   also seems like there may be another issue. uggggghh
+"""
+for i, (sentences, tokenized_labels, mask) in enumerate(val_dataloader):
+    print(sentences[0])
+    print(tokenized_labels[0])
+    print(mask[0])
+    print("________________________")
+    print(tokenized_sentences.numpy())
+    print(labels.numpy())
+    print(mask.numpy)
+    # print(1)
 
 #lets try just hitting it with a dataloader
 
